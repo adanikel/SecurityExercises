4c6acd0e33ed833c98ebd59c0d855aae659a9cda
Daniel Ladi <59119926+adanikel@users.noreply.github.com>
2020-12-23 12:04:40 +0200

    Add files via upload

---
+++ Buffer_overflow_another_buffer.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+
+	char second_buffer[15];
+	char first_buffer[15]; // located below second_buffer on stack frame (assuming memory address increases up)
+
+	/* - Highest address
+	* ....
+	* ========= STACK FRAME
+	* second_buffer
+	* first_buffer
+	* =========
+	* ....
+	* Lowest adress
+	*/
+
+	std::cout << second_buffer - first_buffer << std::endl;
+	// distance is 15 -> writing to first_buffer above 15 will smash into second_buffer
+
+	std::cout << "enter value for first buffer" << std::endl;
+	std::cin >> first_buffer; // enter input: "@@@@@@@@@@@@@@@Conquered"
+
+	std::cout << "current value of second buffer is " << second_buffer; // Will be "Conquered"
+
+	return 0;
+}
